a29b670f6dc6196fa4dd15cb4ceba5f839eeea5d
Shuwen <51765660+sherwinwater@users.noreply.github.com>
2021-08-08 00:11:32 -0400

    Add files via upload

---
+++ assets/data/wanshuweData.ts
@@ -0,0 +1,9 @@
+import { Wanshuwe } from "src/app/type";
+
+export const MYINFO: Wanshuwe = {
+  id: 991583096,
+  name: "Shuwen",
+  campus: "Davis",
+  loginName: "wanshuwe",
+  assignmentTitle: "wanshuweA4",
+};
